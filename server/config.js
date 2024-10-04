50e7deb45b0f26e4bd1ed20cc732c833b3aa04a2
Rowe <130283000+RoweRui@users.noreply.github.com>
2024-10-04 15:31:08 +0800

    Add files via upload

---
+++ server/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://rowe:d0JrJhUBsxPhXfj4@cluster0.i6bpt.mongodb.net/BOOKIEMONSTER?retryWrites=true&w=majority&appName=Cluster0';
+
