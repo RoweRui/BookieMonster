2776b76313614cf361ca76cd3e44be5ee2f8a1ed
Rowe <130283000+RoweRui@users.noreply.github.com>
2024-10-09 14:40:50 +0800

    Update bookModel.js

--- server/models/bookModel.js
+++ server/models/bookModel.js
@@ -14,6 +14,10 @@ const bookSchema = mongoose.Schema(
       type: Number,
       required: true,
     },
+    genre: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
